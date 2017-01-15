2b3e7ad20e9883398bdadaae39f6f6b72815a7ef
Ian Polding <ipolding@gmail.com>
2017-01-15 19:41:34 +0000

    commit

--- src/main/java/uk/co/ipolding/slack/SlackListener.java
+++ src/main/java/uk/co/ipolding/slack/SlackListener.java
@@ -1,29 +1,59 @@
 package uk.co.ipolding.slack;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.util.Assert;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 @PropertySource("classpath:slack.properties")
 public class SlackListener {
 
   private static final Logger log = LoggerFactory.getLogger(SlackListener.class);
 
-  private final RestTemplate restTemplate;
-
   @Value("${slack.api}")
   private String slackApiUrl;
 
-  public SlackListener(RestTemplate restTemplate) {
-    this.restTemplate = restTemplate;
-  }
+  public SlackListener() {
+      }
+
+      private APIResponses.WsInitialization initializeSession() {
+         String apiAuthToken = System.getProperty("slack.api.token");
+         Assert.notNull(apiAuthToken);
+        try {
+          URL url = new URL(slackApiUrl + APIMethods.RTM_API.value + "?token=" + apiAuthToken);
+          URLConnection urlConnection = url.openConnection();
+          urlConnection.connect();
+          byte[] response;
+          try (InputStream inputStream = urlConnection.getInputStream(); ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();) {
+            int data;
+            while ((data = inputStream.read()) != -1) {
+              byteArrayOutputStream.write(data);
+            }
+            response = byteArrayOutputStream.toByteArray();
+            APIResponses.WsInitialization successfulInitialization = new ObjectMapper().readValue(response, APIResponses.WsInitialization.class);
+            if (!successfulInitialization.isOk()) {
+              log.error("FATAL : Couldn't initialize websocket session");
+              throw new ExceptionInInitializerError("Couldn't initialize websocket session");
+            } else {
+              return successfulInitialization;
+            }
+        }} catch (Exception e) {
+          e.printStackTrace();
+          System.exit(1);
+        }
+        return null;
+      }
 
   /** returns the bot id*/
   public String connectToSlack(WebSocketHandler websocketHandler) {
@@ -31,17 +61,6 @@ public class SlackListener {
     APIResponses.WsInitialization initializedSession = initializeSession();
     initializeSession(initializedSession, websocketHandler);
     return initializedSession.getSelf().getId();
-  }
-
-  private APIResponses.WsInitialization initializeSession() {
-    String apiAuthToken = System.getProperty("slack.api.token");
-    Assert.notNull(apiAuthToken);
-    APIResponses.WsInitialization successfulInitialization = restTemplate.getForObject(slackApiUrl + APIMethods.RTM_API.value + "?token={token}", APIResponses.WsInitialization.class, apiAuthToken);
-    if (!successfulInitialization.isOk()) {
-      log.error("FATAL : Couldn't initialize websocket sesstion");
-      System.exit(1);
-    }
-    return successfulInitialization;
   }
 
   private void initializeSession(APIResponses.WsInitialization initializedSession, WebSocketHandler websocketHandler) {
